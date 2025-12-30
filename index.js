7d6e98426d4ad2062118d66b40b98b6f483ec7d9
aditya bastyas <adityabastyasmulya@gmail.com>
2025-12-30 23:13:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
-const students = ["Mikael", "Sarah", "Mikaya", "Budi", "Mamika"];
+const students = [
+  "Mikael",
+  "aDiT",
+  "BasTyAS",
+  "muLya",
+  "sAkuRA",
+  "Sarah",
+  "Mikaya",
+  "Budi",
+  "Mamika",
+];
+
+const chars = {
+  A: "a",
+  B: "b",
+  C: "c",
+  D: "d",
+  E: "e",
+  F: "f",
+  G: "g",
+  H: "h",
+  I: "i",
+  J: "j",
+  K: "k",
+  L: "l",
+  M: "m",
+  N: "n",
+  O: "o",
+  P: "p",
+  Q: "q",
+  R: "r",
+  S: "s",
+  T: "t",
+  U: "u",
+  V: "v",
+  W: "w",
+  X: "x",
+  Y: "y",
+  Z: "z",
+};
 
 const ubahHurufKeKecil = (huruf) => {
-  const kodeHuruf = huruf.charCodeAt(0);
-
-  const hasilHuruf =
-    kodeHuruf >= 65 && kodeHuruf <= 90
-      ? String.fromCharCode(kodeHuruf + 32)
-      : huruf;
-
-  return hasilHuruf;
+  if (chars[huruf]) {
+    return chars[huruf];
+  } else {
+    return huruf;
+  }
 };
 
 const ubahSemuaHuruf = (kata) => {
@@ -22,16 +58,36 @@ const ubahSemuaHuruf = (kata) => {
   return kataBaru;
 };
 
-const apakahCocok = (satu,dua) {
+const apakahCocok = (namaSiswa, kataYangDicari) => {
+  const namaKecil = ubahSemuaHuruf(namaSiswa);
+  const cariKecil = ubahSemuaHuruf(kataYangDicari);
 
-}
+  for (let posisi = 0; posisi < namaKecil.length; posisi++) {
+    let cocok = true;
+
+    for (let j = 0; j < cariKecil.length; j++) {
+      if (namaKecil[posisi + j] !== cariKecil[j]) {
+        cocok = false;
+      }
+    }
+    if (cocok) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+const countResults = (hasil) => {
+  console.log(`Ditemukan ${hasil.length} hasil pencarian`);
+};
 
 const searchStudent = (yangDicari, fungsiCallback) => {
   let hasilCari = [];
-  
+
   for (let i = 0; i < students.length; i++) {
     const nama = students[i];
-    
+
     if (apakahCocok(nama, yangDicari)) {
       hasilCari = [...hasilCari, nama];
     }
